01c125df15ec593958ecd8febc2098ef211d4915
Andrew2a1 <adam.brz@onet.eu>
2020-09-24 13:05:02 +0200

    Change default physical object dimensions

--- DischargeTests/src/tst_PhysicalObject.cpp
+++ DischargeTests/src/tst_PhysicalObject.cpp
@@ -6,6 +6,6 @@ TEST_CASE("PhysicalObject create test", "[PhysicalObject]")
     PhysicalObject physical(1.0);
 
     CHECK(physical.getMass() == 1.0);
-    CHECK(physical.getPosition() == Vector<>({0, 0}));
-    CHECK(physical.getVelocity() == Vector<>({0, 0}));
+    CHECK(physical.getPosition() == Vector<>({0, 0, 0}));
+    CHECK(physical.getVelocity() == Vector<>({0, 0, 0}));
 }
