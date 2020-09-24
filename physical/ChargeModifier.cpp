b8f5cc5d0505443f61e91651a696d2229afa244b
Andrew2a1 <adam.brz@onet.eu>
2020-09-24 13:09:30 +0200

    Add own PI constant

--- physical/ChargeModifier.cpp
+++ physical/ChargeModifier.cpp
@@ -1,10 +1,10 @@
 #include "ChargeModifier.h"
 #include "ElectricCharge.h"
 
-#define _USE_MATH_DEFINES
 #include <cmath>
 
 constexpr double e0 = 8.84e-12;
+constexpr double PI = 3.14159265359;
 
 Vector<double> ChargeModifier::calculateForce(const PhysicalObject *obj, const PhysicalObject *other) const
 {
@@ -18,7 +18,7 @@ Vector<double> ChargeModifier::calculateForce(const PhysicalObject *obj, const P
         const double distance = force.abs();
 
         force *= charge1->getCharge() * charge2->getCharge() /
-                    (4*M_PI*e0 * std::pow(distance, 3));
+                    (4*PI*e0 * std::pow(distance, 3));
     }
 
     return force + PhysicalModifier::calculateForce(obj, other);
