bead766bb7ecfb57c58c1e9623f18c03573bda18
Andrew2a1 <adam.brz@onet.eu>
2020-09-27 12:54:13 +0200

    Add TimeControlWidget class

---
+++ gui/timecontrolwidget.cpp
@@ -0,0 +1,64 @@
+#include "timecontrolwidget.h"
+#include "ui_timecontrolwidget.h"
+#include "simulationwidget.h"
+
+#include <QTimer>
+#include <QPushButton>
+
+TimeControlWidget::TimeControlWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::TimeControlWidget)
+{
+    ui->setupUi(this);
+
+    updateButtons();
+
+    timer = new QTimer(this);
+    timer->setInterval(1000 / 60);
+
+    connect(timer, &QTimer::timeout, this, &TimeControlWidget::timerTimeout);
+    connect(ui->startButton, &QPushButton::clicked, this, &TimeControlWidget::startPressed);
+    connect(ui->pauseButton, &QPushButton::clicked, this, &TimeControlWidget::stopPressed);
+    connect(ui->timeSpinBox, SIGNAL(valueChanged(int)), this, SLOT(updateTimeChanged(int)));
+}
+
+TimeControlWidget::~TimeControlWidget()
+{
+    delete ui;
+}
+
+void TimeControlWidget::setUpdateTarget(SimulationWidget *target)
+{
+    updateTarget = target;
+}
+
+void TimeControlWidget::timerTimeout()
+{
+    if(updateTarget)
+        updateTarget->applyTime(static_cast<double>(updateTime_ms) / 1000.0);
+}
+
+void TimeControlWidget::updateButtons()
+{
+    ui->startButton->setChecked(isUpdating);
+    ui->pauseButton->setChecked(!isUpdating);
+}
+
+void TimeControlWidget::updateTimeChanged(int newTime)
+{
+    updateTime_ms = newTime;
+}
+
+void TimeControlWidget::startPressed()
+{
+    isUpdating = true;
+    timer->start();
+    updateButtons();
+}
+
+void TimeControlWidget::stopPressed()
+{
+    isUpdating = false;
+    timer->stop();
+    updateButtons();
+}
---
+++ gui/timecontrolwidget.h
@@ -0,0 +1,42 @@
+#ifndef TIMECONTROLWIDGET_H
+#define TIMECONTROLWIDGET_H
+
+#include <QWidget>
+
+class SimulationWidget;
+
+namespace Ui {
+class TimeControlWidget;
+}
+
+class TimeControlWidget : public QWidget
+{
+    Q_OBJECT
+
+
+private:
+    Ui::TimeControlWidget *ui;
+    SimulationWidget *updateTarget = nullptr;
+    QTimer *timer;
+
+    int updateTime_ms = 100;
+    bool isUpdating = false;
+
+public:
+    explicit TimeControlWidget(QWidget *parent = nullptr);
+    ~TimeControlWidget();
+
+    void setUpdateTarget(SimulationWidget *target);
+
+private:
+    void timerTimeout();
+    void updateButtons();
+
+private slots:
+    void updateTimeChanged(int newTime);
+
+    void startPressed();
+    void stopPressed();
+};
+
+#endif // TIMECONTROLWIDGET_H
