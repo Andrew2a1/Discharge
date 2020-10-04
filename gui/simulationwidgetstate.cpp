e64f0c46a94d51bf49ef41bb43673d880d9bd3b5
Andrew2a1 <adam.brz@onet.eu>
2020-10-04 15:40:30 +0200

    Remove QObject inheritance  from SimulationWidgetState

--- gui/simulationwidgetstate.cpp
+++ gui/simulationwidgetstate.cpp
@@ -1,13 +1,27 @@
 #include "simulationwidgetstate.h"
 #include "physical/SimulationState.h"
 
-SimulationWidgetState::SimulationWidgetState(QObject *parent) :
-    QObject(parent)
-{
-
-}
-
 SimulationWidgetState::~SimulationWidgetState()
 {
     delete simState;
+}
+
+QList<GraphicObjectPtr> SimulationWidgetState::getGraphicObjects() const
+{
+    return graphicObjects;
+}
+
+void SimulationWidgetState::setGraphicObjects(const QList<GraphicObjectPtr> &value)
+{
+    graphicObjects = value;
+}
+
+SimulationState *SimulationWidgetState::state() const
+{
+    return simState;
+}
+
+void SimulationWidgetState::setState(SimulationState *value)
+{
+    simState = value;
 }
--- gui/simulationwidgetstate.h
+++ gui/simulationwidgetstate.h
@@ -1,23 +1,27 @@
 #ifndef SIMULATIONWIDGETSTATE_H
 #define SIMULATIONWIDGETSTATE_H
 
-#include <QObject>
-#include "simulationwidget.h"
+#include <QList>
+#include "graphicobject.h"
 
 class SimulationState;
 
-class SimulationWidgetState : public QObject
+class SimulationWidgetState
 {
-    Q_OBJECT
-
-public:
+private:
     SimulationState *simState;
-    QList<GraphicObject*> graphicObjects;
+    QList<GraphicObjectPtr> graphicObjects;
 
 public:
-    explicit SimulationWidgetState(QObject *parent = nullptr);
     ~SimulationWidgetState();
 
+    SimulationState *state() const;
+    void setState(SimulationState *value);
+
+    QList<GraphicObjectPtr> getGraphicObjects() const;
+    void setGraphicObjects(const QList<GraphicObjectPtr> &value);
 };
+
+typedef std::shared_ptr<SimulationWidgetState> SimulationWidgetStatePtr;
 
 #endif // SIMULATIONWIDGETSTATE_H
